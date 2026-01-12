3b05b9796a074d6a8886e5c2960d395c87ed4b9f
Me0wzz <cjwoo1016@gmail.com>
2026-01-12 14:17:30 +0900

    feat(system): implement SP804 timer and uptime command

--- test.c
+++ test.c
@@ -1,5 +1,34 @@
 volatile unsigned int *const UART0DR = (unsigned int *)0x101f1000;
 volatile unsigned int *const UART0FR = (unsigned int *)0x101f1018;
+
+// SP804 Timer Initialization
+volatile unsigned int *const TIMER0_BASE = (unsigned int *)0x101E2000;
+volatile unsigned int *const TIMER0_LOAD =
+    (unsigned int *)0x101E2000; // Offset 0x00
+volatile unsigned int *const TIMER0_VALUE =
+    (unsigned int *)0x101E2004; // Offset 0x04
+volatile unsigned int *const TIMER0_CONTROL =
+    (unsigned int *)0x101E2008; // Offset 0x08
+
+#define TIMER_MAX_VAL 0xFFFFFFFF
+
+void timer_init() {
+  *TIMER0_CONTROL = 0;
+
+  *TIMER0_LOAD = TIMER_MAX_VAL; // Load value
+  // Bit 7: Enable interrupt
+  // Bit 6: Periodic mode / Free-running mode
+  // Bit 1: Prescale value (1 = 32-bit, 0 = 16-bit)
+  // 1000 0010 = 0x82
+  *TIMER0_CONTROL = 0x82;
+}
+
+unsigned int uptime() {
+  unsigned int current_value = *TIMER0_VALUE;
+  unsigned int elapsed = TIMER_MAX_VAL - current_value;
+  return elapsed / 1000000;
+}
+
 void print_uart0(const char *s) {
   while (*s != '\0') {
     *UART0DR = (unsigned int)(*s);
@@ -8,6 +37,22 @@ void print_uart0(const char *s) {
 }
 
 void putc_uart0(char c) { *UART0DR = (unsigned int)c; }
+
+void print_dec(unsigned int num) {
+  if (num == 0) {
+    putc_uart0('0');
+    return;
+  }
+  char buf[10];
+  int i = 0;
+  while (num > 0) {
+    buf[i++] = (char)((num % 10) + '0');
+    num /= 10;
+  }
+  for (int j = i - 1; j >= 0; j--) {
+    putc_uart0(buf[j]);
+  }
+}
 
 int strcmp(const char *s1, const char *s2) {
   while (*s1 != '\0' && (*s1 == *s2)) {
@@ -18,6 +63,7 @@ int strcmp(const char *s1, const char *s2) {
 }
 
 void c_entry() {
+  timer_init();
   char c;
   char cmd_buf[15];
   unsigned int buf_idx = 0;
@@ -34,10 +80,16 @@ void c_entry() {
           print_uart0("help - Show this message\n");
           print_uart0("whoami - Show the current user\n");
           print_uart0("clear - Clear the screen\n");
+          print_uart0("uptime - Show system uptime\n");
         } else if (strcmp(cmd_buf, "whoami") == 0) {
           print_uart0("You are user 'ghost'\n");
         } else if (strcmp(cmd_buf, "clear") == 0) {
           print_uart0("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        } else if (strcmp(cmd_buf, "uptime") == 0) {
+          print_uart0("Uptime: ");
+          unsigned int up = uptime();
+          print_dec(up);
+          print_uart0(" seconds\n");
         } else {
           print_uart0("Unknown command: ");
           print_uart0(cmd_buf);
